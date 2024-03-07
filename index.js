8d5902ce2dcfbe6f1f5c9a4d7c032dba43126a67
vikas <vikas@synswift.com>
2024-03-07 10:07:09 +0530

    mongoose added

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
-const cors = require ('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const server = express()
+main().catch((err) => console.log(err));
 
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/CHM-Copy");
+    console.log('DB Connected')
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+const userSchema = new mongoose.Schema({
+    Email: String,
+    Password : String
+  });
+
+  const User = mongoose.model('User', userSchema);
+
+
+const server = express();
 
 server.use(cors());
 server.use(bodyParser.json());
 
-server.post('/home',(req, res) => {
-    console.log(req.body);
-    res.json(req.body);
-})
+server.post("/home", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
-server.listen (8080,()=>{
-    console.log('Server Started');
-})
+server.listen(8080, () => {
+  console.log("Server Started");
+});
