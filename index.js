72418cdb3588efdc8961fc42f081487dd5303b40
vikas <vikas@synswift.com>
2024-03-07 17:15:03 +0530

    1st api created

--- index.js
+++ index.js
@@ -7,27 +7,41 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/CHM-Copy");
-    console.log('DB Connected')
+  console.log("DB Connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-
-const userSchema = new mongoose.Schema({
-    Email: String,
-    Password : String
-  });
-
-  const User = mongoose.model('User', userSchema);
-
-
 const server = express();
 
-server.use(cors());
-server.use(bodyParser.json());
+server.use(cors(
+   { credentials:true}
+));
+server.use(bodyParser.json({limit:"200mb"}));
+server.use(bodyParser.urlencoded({limit:"200mb",extended:true,parameterLimit:1000000}));
 
-server.post("/home", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+
+server.post("/home", async (req, res) => {
+    console.log('hearhhhhhh')
+    try {
+        let user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+      
+        const doc = await user.save();
+        console.log('doc',doc);
+       return res.status(200).json(doc);
+    } catch (error) {
+        console.log('errr',error)
+    }
+
 });
 
 server.listen(8080, () => {
